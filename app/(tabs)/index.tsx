371d6c07bd351b6da64314a8c506e33af7c34838
FroskGamer <mbkarlsen08@outlook.com>
2025-10-22 08:27:25 +0200

    Første commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,113 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { Text, View, StyleSheet,} from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+   	<View style={styles.boxRød}>
+    		<Text style={styles.tittel}>En app</Text>
+    	</View>
+
+		<View>
+			<View style={styles.div2}>
+
+				<View style={styles.boxGrønn}/> 
+				<View style={styles.boxSvart}/>
+
+			</View>
+
+			<View style={styles.div3}>
+				<View style={styles.boxGul}/>
+			</View>
+		</View>
+
+		<View style={styles.tekstBoks}>
+			<Text style={styles.text}>Tekst some er uten mening!</Text>
+		</View>
+		
+		<View style={styles.blåKnapp}>
+			<Text style={styles.tekstOK}>OK</Text>
+		</View>
+
+
+	</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+
+	tekstBoks: {
+		width: "75%",
+		alignItems: "center",
+		marginLeft: "auto",
+		marginRight: "auto",
+		marginTop: 40,
+	},
+
+	text: {
+		fontSize: 27.27,
+		color: "rgba(0, 0, 0, 1)",
+	},
+
+	boxRød: {
+		width: "100%",
+		height: 150,
+		backgroundColor: "rgba(165, 72, 72, 1)",
+		justifyContent: "center",
+		alignItems: "center",    
+	},
+
+	tittel: {
+		fontSize: 40,
+		color: "rgba(255, 255, 255, 1)",
+	},
+
+	div2: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+
+	div3: {
+		alignItems: "center",
+	},
+
+	boxGrønn: {
+		backgroundColor: "rgba(73, 190, 103, 1)",
+		width: 75,
+		height: 75,
+		marginLeft: 25,
+	},
+
+	boxSvart: {
+		backgroundColor: "rgba(50, 58, 52, 1)",
+		width: 75,
+		height: 75,
+		marginRight: 25,
+	},
+
+   	boxGul: {
+		backgroundColor: "rgba(174, 185, 67, 1)",
+		width: 75,
+		height: 75,
+
+	},
+
+	blåKnapp: {
+		backgroundColor: "rgba(42, 53, 180, 1)",
+		width: 150,
+		height: 75,
+		marginTop: "40%",
+		marginLeft: "auto",
+		marginRight: "auto",
+		borderRadius: 10,
+		justifyContent: "center",
+		alignItems: "center",
+		
+
+	},
+	tekstOK: {
+		fontSize: 40,
+		color: "rgba(255, 255, 255, 1)",
+
+	}
 });
